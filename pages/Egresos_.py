fdd3a21523535f8c8b2b27353108a333fb1c92d7
Andres Visco Bonomo <visco.andres@gmail.com>
2023-09-07 05:27:12 -0300

    v3

--- pages/Egresos_.py
+++ pages/Egresos_.py
@@ -2,10 +2,13 @@ import streamlit as st
 from egresos import Egresos
 import pandas as pd
 import sqlite3
+from datetime import datetime
 
 _descripcion = st.text_input("Descripcion")
 _importe = st.number_input("Importe")
-_fecha = st.date_input("Fecha")
+_fecha = st.date_input("Fecha", datetime.date(datetime.now()))
+_time = st.time_input("Hora", datetime.time(datetime.now()))
+_fecha = datetime.combine(_fecha, _time)
 _tipo = st.selectbox("Tipo", ["General", "Gasto Materiales", "Adelanto"])# Socio", "Ingreso Negocio", "Otro"])
 _destino = st.text_input("Destino")
 
